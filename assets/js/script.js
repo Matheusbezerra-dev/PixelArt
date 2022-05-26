8c336ec53588e314570742ead609c4bc637c20a2
Matheus Bezerra <mabezerra2@gmail.com>
2022-05-26 16:26:21 -0400

    finalização do exercicio 8

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
-let defaultColor = document.getElementsByClassName('color')[0];
-defaultColor.classList.add('selected');
+// initial Data
+const pixel = document.getElementsByClassName('pixel');
+const firstColor = 'black';
 
-let firstColor = 'black'
+// Events
 document.querySelectorAll('#color-palette .color').forEach(item => {
   item.addEventListener('click', colorClickEvent);
 });
 
+// functions
+window.onload = function initialSelected(){
+  const defaultColor = document.getElementsByClassName('color')[0];
+  defaultColor.classList.add('selected');
+};
+
 function colorClickEvent(e) {
-  let color = e.target.getAttribute('data-color');
-    firstColor = color;
+  const color = e.target.getAttribute('data-color');
+  // firstColor = color;
   document.querySelector('.color.selected').classList.remove('selected');
   e.target.classList.add('selected');
 };
+
+function colorPixel(){
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener('click', () => {
+      pixel[i].style.backgroundColor = window
+        .getComputedStyle(document.getElementsByClassName('selected')[0])
+        .getPropertyValue('background-color');
+    });
+  };  
+};colorPixel()
